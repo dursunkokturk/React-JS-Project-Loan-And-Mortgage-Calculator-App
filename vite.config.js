f98421dd62869de999799b63b0d8c0a6aa9fd7a4
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-15 19:54:58 +0300

    projeyi canliya almak icin package.lock.json, package.json, vite.config.js dosyalarinda gerekli islemler yapildi

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/React-JS-Project-Loan-And-Mortgage-Calculator-App/',
   plugins: [react()],
 })
